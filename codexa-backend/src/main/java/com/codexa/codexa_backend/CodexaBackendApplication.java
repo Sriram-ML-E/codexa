c739945d97b7b044ac0d70163a65a7f199087fdc
Rsriram13 <srirampilot77@gmail.com>
2026-05-11 22:14:04 +0530

    feat: initialize codexa-backend project with Spring Boot and PostgreSQL

--- codexa-backend/src/main/java/com/codexa/codexa_backend/CodexaBackendApplication.java
+++ codexa-backend/src/main/java/com/codexa/codexa_backend/CodexaBackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package com.codexa.codexa_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class CodexaBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(CodexaBackendApplication.class, args);
 	}
 
 }
